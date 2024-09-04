9f0ff0ffb685c63e7d700c4b9b630184093c7983
ekahanny <ekahnny@gmail.com>
2024-09-04 09:45:21 +0800

    feat: create UI article

--- resources/views/components/navlink.blade.php
+++ resources/views/components/navlink.blade.php
@@ -1,8 +1,9 @@
+@props(['active' => false])
+
 <a 
     {{-- variabel attributes digunakan utk mengambil atribut yg akan digunakan pada component  --}}
     {{ $attributes }} 
-    class="{{ $active ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white' }} 
-            rounded-md px-3 py-2 text-sm font-medium"
+    class="{{ $active ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white' }} rounded-md px-3 py-2 text-sm font-medium"
     aria-current="page">
     {{ $slot }}
 </a>
