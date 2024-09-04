2d3fc1ee37008d7e2d62e431a47718c673c25376
ekahanny <ekahnny@gmail.com>
2024-09-04 17:30:30 +0800

    feat: create slug for url

--- routes/web.php
+++ routes/web.php
@@ -16,12 +16,14 @@ Route::get('/posts', function () {
     return view('posts', ["title" => "Blog", "posts" => [
         [
             'id' => 1,
+            'slug' => 'judul-artikel-1',
             'title' => 'Judul Artikel 1',
             'author' => 'Eka Hanny',
             'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem dolores eum quam sunt laudantium quidem pariatur repellat, suscipit rerum molestias ad perferendis perspiciatis reiciendis magnam. Facilis totam nam cumque soluta.'
         ],
         [
             'id' => 2,
+            'slug' => 'judul-artikel-2',
             'title' => 'Judul Artikel 2',
             'author' => 'Eka Hanny',
             'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi laborum magni fuga alias qui? Ipsam, ratione dolorum. Repellendus, libero iure inventore a iusto voluptatum quidem. Architecto voluptates quasi dolores distinctio.'
@@ -29,25 +31,27 @@ Route::get('/posts', function () {
     ]] );
 });
 
-Route::get('/posts/{id}', function($id) {
+Route::get('/posts/{slug}', function($slug) {
     $posts = [
         [
             'id' => 1,
+            'slug' => 'judul-artikel-1',
             'title' => 'Judul Artikel 1',
             'author' => 'Eka Hanny',
             'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem dolores eum quam sunt laudantium quidem pariatur repellat, suscipit rerum molestias ad perferendis perspiciatis reiciendis magnam. Facilis totam nam cumque soluta.'
         ],
         [
             'id' => 2,
+            'slug' => 'judul-artikel-2',
             'title' => 'Judul Artikel 2',
             'author' => 'Eka Hanny',
             'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi laborum magni fuga alias qui? Ipsam, ratione dolorum. Repellendus, libero iure inventore a iusto voluptatum quidem. Architecto voluptates quasi dolores distinctio.'
         ],
     ];
 
-    // Menemukan elemen pertama dalam array posts yang memiliki id yg sama
-    $post = Arr::first($posts, function($post) use ($id) {
-        return $post['id'] == $id;
+    // Menemukan elemen pertama dalam array posts yang memiliki slug yg sama
+    $post = Arr::first($posts, function($post) use ($slug) {
+        return $post['slug'] == $slug;
     });
 
     return view('post', ['title' => 'Detail Post', 'post' => $post]);
