60e3a90d5327c57eb5c0b92e0b839e1329c25d26
ekahanny <ekahnny@gmail.com>
2024-09-13 13:54:29 +0800

    database seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,9 +2,13 @@
 
 namespace Database\Seeders;
 
+use App\Models\Category;
+use App\Models\Post;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,11 +17,23 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        // setiap kali seeder dijalankan, user ini akan dimasukkan ke db
+        $user_admin = User::create([
+            'name' => 'Admin',
+            'username' => "admin123",
+            'email' => "admin@gmail.com",
+            'email_verified_at' => now(),
+            'isAdmin' => true,
+            'password' => Hash::make('admin12345'),
+            'remember_token' => Str::random(10)
+
         ]);
+
+        Post::factory(100)->recycle([
+            Category::factory(5)->create(),
+            $user_admin,
+            User::factory(10)->create()
+        ])->create();
     }
 }
--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -3,8 +3,14 @@
   
         <article class="py-8 max-w-screen-md">
             <h2 class="mb-2 text-3xl font-semibold tracking-tight text-gray-900">{{$post['title']}}</h2>
-            <div class="text-base text-gray-500">
-                <a href="/authors/{{ $post->author->id }}">{{ $post->author->name }}</a> | 1 Januari 2024
+            <div>
+                By
+                <a href="/authors/{{ $post->author->username }}" class="hover:underline  text-base text-gray-500">{{ $post->author->name }}</a>
+                 in 
+                <a href="/category/{{ $post->category->slug }}" class="hover:underline text-base text-gray-500">{{ $post->category->name }}</a>
+                 |
+                 {{-- php date format --}}
+                {{ $post['created_at']->format('j F Y') }}
             </div>
             <p class="my-4 font-light">{{ $post['body'] }}</p>
             <a href="/posts" class="font-medium text-blue-500 hover:underline"> &laquo; Back to posts </a>
